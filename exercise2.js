7c1cf5b88352788c952a68bae75235b7ba8ecefd
Linidri69 <32621455+Linidri69@users.noreply.github.com>
2017-11-27 23:04:21 +0000

    Three bar menu
    In HTML: Added three bar menu using only HTML. Created hoover button with hamburger icon.
    
    In CSS: Styled button

---
+++ exercise2.js
@@ -0,0 +1,6 @@
+jQuery(document).ready(function($){
+  $("li.content").hide();
+  $("ul.toggle-menu").delegate("li.toggle", "click", function() { 
+  $(this).next().toggle("fast").siblings(".content").hide("fast");
+    });
+});
