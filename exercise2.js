ca302420bd2e28c9889de67ecf69d65d9568b655
Linidri69 <32621455+Linidri69@users.noreply.github.com>
2017-12-04 18:46:35 +0000

    Social Media
    IT LINKS FAB

--- exercise2.js
+++
@@ -1,6 +0,0 @@
-jQuery(document).ready(function($){
-  $("li.content").hide();
-  $("ul.toggle-menu").delegate("li.toggle", "click", function() { 
-  $(this).next().toggle("fast").siblings(".content").hide("fast");
-    });
-});
